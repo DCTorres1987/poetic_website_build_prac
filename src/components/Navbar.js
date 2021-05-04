6ab7976449094eb3faa1a0fbbfceb27cd208b064
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-05-03 21:11:20 -0400

    work on building front end. starting with navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+const Navbar = () => (
+
+
+    <section className="nav-bar-section">
+     <img src='./images/poetic-logo.PNG' alt='poetic-logo'/>
+    </section>
+
+
+
+)
+
+export default Navbar;
