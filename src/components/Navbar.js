5f7456aeb22d183f0c8ad2db55f3298fe9373937
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-05-04 23:21:18 -0400

    work on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
+import React from 'react';
 
-const Navbar = () => (
+// import { 
+// } from '../styled/navbarStyled'
+
+const Navbar = () => {
+
+    const links = ['Work', 'Services', 'Company', 'News', 'Contact']
+
+    return (
+
+         <section className="nav-bar-section">
+            <img src='./images/poetic-logo.PNG' alt='poetic-logo'/>
+            <ul>
+            {links.map((value, idx) => (
+                <navbar key={Math.random().toString(36).substr(2, 9)}>{value}</navbar>
+
+            ))}
+            </ul>
+         </section>
+
+    )
 
 
-    <section className="nav-bar-section">
-     <img src='./images/poetic-logo.PNG' alt='poetic-logo'/>
-    </section>
+   
 
 
 
-)
+}
 
 export default Navbar;
